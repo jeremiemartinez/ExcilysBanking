b2aeea5a036e84ad0d58993211926823cc795655
Jeremie Martinez <jeremiemartinez@gmail.com>
2012-06-19 12:07:21 +0200

    Test working !

--- excilysbanking-dao-impl/src/test/java/com/excilys/excilysbanking/dao/UserDAOTest.java
+++ excilysbanking-dao-impl/src/test/java/com/excilys/excilysbanking/dao/UserDAOTest.java
@@ -1,12 +1,13 @@
 
 package com.excilys.excilysbanking.dao;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -16,18 +17,19 @@ import com.excilys.ebi.spring.dbunit.test.DataSet;
 import com.excilys.ebi.spring.dbunit.test.DataSetTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/context/appcontext-dao-test.xml")
+@ContextConfiguration("classpath:context/appcontext-dao-test.xml")
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DataSetTestExecutionListener.class })
-@DataSet(value = "/datasets/usersDataSet.xml", dbType = DBType.H2)
+@DataSet(locations = "/datasets/datasetUser.xml", dbType = DBType.H2)
 @TransactionConfiguration
 @Transactional
-public class UserDAOTest {
+public class UserDAOTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	private UserDAO userDAOTest;
 
 	@Test
-	public void testSomethingWithDao() {
-		Assert.assertTrue(true);
+	public void findByLoginTest() {
+		assertEquals(userDAOTest.findUserByLogin("jmartinez").getNationality(), "French");
+		assertEquals(userDAOTest.findUserByLogin("lponnau").getPhone(), "0836656565");
 	}
 }

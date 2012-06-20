a104f21e0c0bc7f9aa3a2b257482a22fb83d89b1
Luc Ponnau <luc.ponnau@gmail.com>
2012-06-20 11:46:40 +0200

    Updated postgres SQL scheme

--- src/flyway/migration/V1__user_table.sql
+++ src/flyway/migration/V1__user_table.sql
@@ -1,4 +1,23 @@
-CREATE TABLE IF NOT EXISTS users (login VARCHAR(16) PRIMARY KEY, password VARCHAR(20) NOT NULL, name VARCHAR(80), phone VARCHAR(16), nationality VARCHAR(30));
+CREATE TABLE IF NOT EXISTS authorities (
+	authority_id integer PRIMARY KEY,
+	role ENUM('ROLE_USER', 'ROLE_ADMIN')
+);
 
-INSERT INTO users (login, password, name, phone, nationality) VALUES('jmartinez', 'excilys', 'Martinez Jeremie', '0652776432', 'French');
-INSERT INTO users (login, password, name, phone, nationality) VALUES('lponnau', 'bizu', 'Ponnau Luc', '0836656565', 'French'); 
+CREATE TABLE IF NOT EXISTS users (
+	username VARCHAR(50) PRIMARY KEY, 
+	password VARCHAR(150) NOT NULL, 
+	firstname VARCHAR(150) NOT NULL,
+	lastname VARCHAR(150) NOT NULL,
+	authority_id integer references authorities(authority_id)
+);
+
+CREATE TABLE IF NOT EXISTS comptes (
+	compte_id integer PRIMARY KEY,
+	solde double precision NOT NULL,
+	username_id VARCHAR(50) references users(username_id)
+);
+
+INSERT INTO authorities (1, 'ROLE_USER')
+INSERT INTO authorities (2, 'ROLE_ADMIN')
+INSERT INTO users (login, password, name, phone, nationality, fk_authority) VALUES('lponnau', 'excilys', 'Ponnau Luc', 1); 
+INSERT INTO users (login, password, name, phone, nationality, fk_authority) VALUES('jmartinez', 'excilys', 'Martinez Jeremie', 2);

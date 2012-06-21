8802b4f49eb2e86bf2dc4d80259bb36a02121dca
Luc Ponnau <luc.ponnau@gmail.com>
2012-06-21 15:10:54 +0200

    Fix the 403 and you're free

--- excilysbanking-entities/src/main/java/com/excilys/excilysbanking/entities/Compte.java
+++ excilysbanking-entities/src/main/java/com/excilys/excilysbanking/entities/Compte.java
@@ -13,67 +13,67 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "comptes")
 public class Compte {
-
+	
 	public enum CompteType {
 		CARTE, ESPECE;
 	}
-
+	
 	@Id
 	@Column
 	private Integer compte_id;
-
+	
 	@Column
 	private Double solde;
-
+	
 	@Enumerated(EnumType.STRING)
 	@Column
 	private CompteType type;
-
+	
 	@ManyToOne
 	@JoinColumn(name = "username", nullable = false)
 	private User user;
-
+	
 	public Compte() {}
-
+	
 	public Compte(Integer compte_id, Double solde, CompteType type, User user) {
 		this.compte_id = compte_id;
 		this.solde = solde;
 		this.type = type;
 		this.user = user;
 	}
-
+	
 	public Integer getCompte_id() {
 		return compte_id;
 	}
-
+	
 	public void setCompte_id(Integer compte_id) {
 		this.compte_id = compte_id;
 	}
-
+	
 	public Double getSolde() {
 		return solde;
 	}
-
+	
 	public void setSolde(Double solde) {
 		this.solde = solde;
 	}
-
+	
 	public CompteType getType() {
 		return type;
 	}
-
+	
 	public void setType(CompteType type) {
 		this.type = type;
 	}
-
+	
 	public User getUser() {
 		return user;
 	}
-
+	
 	public void setUser(User user) {
 		this.user = user;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -102,13 +102,12 @@ public class Compte {
 			return false;
 		return true;
 	}
-
+	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Compte [compte_id=").append(compte_id).append(", solde=").append(solde).append(", compteType=").append(type).append(", username=")
-				.append(user).append("]");
+		builder.append("Compte [compte_id=").append(compte_id).append(", solde=").append(solde).append(", compteType=").append(type).append(user).append("]");
 		return builder.toString();
 	}
-
+	
 }

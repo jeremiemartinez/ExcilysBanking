1309aca8caa144fd4f6fc8c68c4bb41c995046fb
Luc Ponnau <luc.ponnau@gmail.com>
2012-06-28 11:28:32 +0200

    Added previous months

--- excilysbanking-web/src/main/java/com/excilys/excilysbanking/web/controllers/OperationsController.java
+++ excilysbanking-web/src/main/java/com/excilys/excilysbanking/web/controllers/OperationsController.java
@@ -4,6 +4,7 @@ package com.excilys.excilysbanking.web.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import org.joda.time.DateTime;
+import org.joda.time.YearMonth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -30,34 +31,17 @@ public class OperationsController {
 			m.addAttribute("isAdmin", "true");
 		
 		DateTime now = DateTime.now();
+		YearMonth ym = new YearMonth(now.getYear(), now.getMonthOfYear());
 		m.addAttribute("id", id);
-		m.addAttribute("carteSum", operationService.getMontantOperationsCarteByCompteIdAndYearMonth(id, year, month));
-		m.addAttribute("operationsList", operationService.getOperationsVirementByCompteIdAndYearMonth(id, year, month));
-		m.addAttribute("operationsCarteList", operationService.getOperationsCarteByCompteIdAndYearMonth(id, year, month));
+		m.addAttribute("carteSum", operationService.getMontantOperationsCarteByCompteIdAndYearMonth(id, ym));
+		m.addAttribute("operationsList", operationService.getOperationsVirementByCompteIdAndYearMonth(id, ym));
+		m.addAttribute("operationsCarteList", operationService.getOperationsCarteByCompteIdAndYearMonth(id, ym));
 		
 		List<String> prevMonths = new ArrayList<String>(5);
 		for (int i = 0; i < 5; i++)
-			prevMonths.add("month" + (now = now.plusMonths(1)).getMonthOfYear());
+			prevMonths.add("month" + (now = now.minusMonths(1)).getMonthOfYear());
 		m.addAttribute("previousMonths", prevMonths);
 		
 		return "/secured/operations";
-	}
-	
-	private String calculatePreviousDate(Integer year, Integer month) {
-		StringBuilder sb = new StringBuilder();
-		if (month.equals(new Integer(1)))
-			sb.append(year - 1).append("_").append(12);
-		else
-			sb.append(year).append("_").append(month - 1);
-		return sb.toString();
-	}
-	
-	private String calculateLaterDate(Integer year, Integer month) {
-		StringBuilder sb = new StringBuilder();
-		if (month.equals(new Integer(12)))
-			sb.append(year + 1).append("_").append(1);
-		else
-			sb.append(year).append("_").append(month + 1);
-		return sb.toString();
 	}
 }

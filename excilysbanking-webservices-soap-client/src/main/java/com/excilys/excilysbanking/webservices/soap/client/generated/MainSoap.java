67e229007dc1878f55eaacd9d9a9d9297b8e19fb
Jeremie Martinez <jeremiemartinez@gmail.com>
2012-07-05 15:00:14 +0200

    client implementation soap

---
+++ excilysbanking-webservices-soap-client/src/main/java/com/excilys/excilysbanking/webservices/soap/client/generated/MainSoap.java
@@ -0,0 +1,11 @@
+package com.excilys.excilysbanking.webservices.soap.client.generated;
+
+
+public class MainSoap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

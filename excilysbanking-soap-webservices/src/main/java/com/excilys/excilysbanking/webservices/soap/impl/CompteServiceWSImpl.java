68656f7d462528f3c5984211098e0f110b93e9e4
Jeremie Martinez <jeremiemartinez@gmail.com>
2012-07-05 12:26:59 +0200

    Correcting shit and deleting soap webservices failed

---
+++ excilysbanking-soap-webservices/src/main/java/com/excilys/excilysbanking/webservices/soap/impl/CompteServiceWSImpl.java
@@ -0,0 +1,44 @@
+
+package com.excilys.excilysbanking.webservices.soap.impl;
+
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import com.excilys.excilysbanking.dto.CompteDTO;
+import com.excilys.excilysbanking.dto.converters.Converter;
+import com.excilys.excilysbanking.entities.Compte;
+import com.excilys.excilysbanking.services.CompteService;
+import com.excilys.excilysbanking.webservices.soap.CompteServiceWS;
+
+@WebService(endpointInterface = "com.excilys.excilysbanking.webservices.soap.CompteServiceWS")
+public class CompteServiceWSImpl implements CompteServiceWS {
+
+	@Autowired
+	@Qualifier("compteToCompteDTOConverter")
+	private Converter<Compte, CompteDTO> converter;
+
+	@Autowired
+	private CompteService compteService;
+
+	@Override
+	@WebMethod
+	public List<CompteDTO> getComptesByUsername(@WebParam String username) {
+		return converter.convert(compteService.getComptesByUsername(username));
+	}
+
+	@Override
+	@WebMethod
+	public List<CompteDTO> getAllComptes() {
+		return converter.convert(compteService.getAllComptes());
+	}
+
+	@Override
+	@WebMethod
+	public CompteDTO getCompteById(@WebParam Integer id) {
+		return converter.convert(compteService.getCompteById(id));
+	}
+
+}

ff093b4928d3166f8e903931e133a7010727c489
Luc Ponnau <luc.ponnau@gmail.com>
2012-07-05 09:35:48 +0200

    Fixed mhmmmmmmmmmmm

--- excilysbanking-web/src/main/java/com/excilys/excilysbanking/web/controllers/OperationsController.java
+++ excilysbanking-web/src/main/java/com/excilys/excilysbanking/web/controllers/OperationsController.java
@@ -10,6 +10,7 @@ import java.util.Locale;
 import org.joda.time.DateTime;
 import org.joda.time.YearMonth;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,67 +26,68 @@ import com.excilys.excilysbanking.services.OperationService;
 @Controller
 @RequestMapping("/secured")
 public class OperationsController {
-
+	
 	@Autowired
 	private OperationService operationService;
-
+	
 	@Autowired
+	@Qualifier("operationToOperationDTOConverter")
 	private Converter<Operation, OperationDTO> converter;
-
+	
 	// Operations Virement Paging used by JSP
-
+	
 	@RequestMapping("/operations/id/{id}")
 	public String operations(Model m, @PathVariable Integer id) {
 		return operations(m, id, DateTime.now().getYear(), DateTime.now().getMonthOfYear(), 1);
 	}
-
+	
 	@RequestMapping("/operations/id/{id}/year/{year}/month/{month}")
 	public String operations(Model m, @PathVariable Integer id, @PathVariable Integer year, @PathVariable Integer month) {
 		return operations(m, id, year, month, 1);
 	}
-
+	
 	@RequestMapping("/operations/id/{id}/year/{year}/month/{month}/page/{page}")
 	public String operations(Model m, @PathVariable Integer id, @PathVariable Integer year, @PathVariable Integer month, @PathVariable Integer page) {
-
+		
 		// Months navigation
 		YearMonth ym = new YearMonth(year, month);
 		m.addAttribute("requestedMonth", ym);
 		m.addAttribute("months", getPreviousMonths());
 		m.addAttribute("month", month);
 		m.addAttribute("year", year);
-
+		
 		// Infos
 		m.addAttribute("id", id);
 		m.addAttribute("carteSum", operationService.getMontantOperationsCarte(id, ym));
-
+		
 		// Pages navigation
 		Long totalOperations = operationService.getNumberOperations(id, Operation.OperationType.VIREMENT, ym);
 		long lastPage = totalOperations / VIREMENTS_PER_PAGE;
 		if (totalOperations % VIREMENTS_PER_PAGE != 0)
 			lastPage++;
-
+		
 		// Creating Model
 		m.addAttribute("operationsList", operationService.getOperations(id, OperationType.VIREMENT, ym, VIREMENTS_PER_PAGE, page));
 		addPagingInformation(m, page, lastPage);
-
+		
 		return "/secured/operations";
 	}
-
+	
 	// Operations Carte Paging used by AJAX
-
+	
 	@RequestMapping("/operations/id/{id}/year/{year}/month/{month}/cartes")
 	public @ResponseBody
 	List<OperationDTO> operationsCarteJSON(Locale l, @PathVariable Integer id, @PathVariable Integer year, @PathVariable Integer month) {
 		return operationsCarteJSON(l, id, year, month, 1);
 	}
-
+	
 	@RequestMapping("/operations/id/{id}/year/{year}/month/{month}/cartes/pages")
 	public @ResponseBody
 	Long numberOperationsCarteJSON(Locale l, @PathVariable Integer id, @PathVariable Integer year, @PathVariable Integer month) {
 		CustomDateSerializer.setLocale(l);
 		return operationService.getNumberOperations(id, OperationType.CARTE, new YearMonth(year, month));
 	}
-
+	
 	@RequestMapping("/operations/id/{id}/year/{year}/month/{month}/cartes/page/{page}")
 	public @ResponseBody
 	List<OperationDTO> operationsCarteJSON(Locale l, @PathVariable Integer id, @PathVariable Integer year, @PathVariable Integer month,
--- excilysbanking-web/src/main/webapp/WEB-INF/jsp/included/header.jsp
+++ excilysbanking-web/src/main/webapp/WEB-INF/jsp/included/header.jsp
@@ -1,4 +1,4 @@
-<a href="/ebank/index"><img style="text-align:center" src="/ebank/resources/img/logo.png" /></a>
+<a href="/ebank/index"><img style="text-align:center" src="/ebank/resources/img/logo.svg" /></a>
 
 <div id="container">
 <div id="content">
